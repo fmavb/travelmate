df3693c6428d7239390393a399662efa008c0a7d
magnarch <magnarch@gmail.com>
2018-03-09 21:10:01 +0000

    removing useless function added by mistake in previous commit

--- app/models.py
+++ app/models.py
@@ -27,8 +27,6 @@ class UserProfile(models.Model):
 			"lat": self.homeCountry.latitude,
 			"lng": self.homeCountry.longitude,
 		}
-	def get_homeCountry(self):
-		return homeCountry
 
 class Trip(models.Model):
 	owner = models.ForeignKey(User,on_delete=models.CASCADE)
