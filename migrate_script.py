dd567e683d1c163a4bedd9d2300358271cc8638b
Alexander Behrmann <alexander.behrmann@hotmail.com>
2018-10-13 19:53:02 -0700

    Migration script created

---
+++ migrate_script.py
@@ -0,0 +1,28 @@
+import os, shutil
+from datetime import datetime
+
+from django.core.files.images import ImageFile
+from django.core.management import execute_from_command_line
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'travelmate.settings')
+
+import django
+
+django.setup()
+import random
+import csv
+from django.contrib.auth.models import User
+from app.models import Destination, UserProfile, Trip, BlogPost, Comment, Rating
+
+
+def migration():
+    trips = Trip.objects.all()
+    for trip in trips:
+        owner = Trip.owner
+        trip.origin = owner.homeCountry
+
+
+if __name__ == '__main__':
+    print("Running migration script")
+    migration()
